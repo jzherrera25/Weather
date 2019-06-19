55283484c3c658e01195b3692d2e87921d753218
Joshua Herrera <joshz8462@gmail.com>
2019-06-18 21:33:13 -0700

    Added hamburger icon to toolbar/navbar by enabling it.

--- app/src/main/java/com/example/weather/Activities/WeatherActivity.kt
+++ app/src/main/java/com/example/weather/Activities/WeatherActivity.kt
@@ -3,15 +3,34 @@ package com.example.weather.Activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import com.example.weather.R
 import com.example.weather.WeatherRepository
 
-class WeatherActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.weather_drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
 
         val weatherRepository: WeatherRepository = WeatherRepository.getInstance()
         weatherRepository.doAction()
@@ -20,5 +39,19 @@ class WeatherActivity : AppCompatActivity() {
         testButton.setOnClickListener {
             weatherRepository.doAction()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.weather_drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
